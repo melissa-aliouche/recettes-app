dadc0f029eda73247ec61d2c97599c8304909b3a
Aliouche Melissa <166093751+melissa-aliouche@users.noreply.github.com>
2025-04-20 19:44:58 +0200

    Update header.php

--- view/templates/header.php
+++ view/templates/header.php
@@ -5,7 +5,6 @@
     <title>Site de recettes</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
